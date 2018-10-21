d012c343656f24a74d9b5bfe2563b8fa16b8fccc
ivyhjk <elvis.munoz.f@gmail.com>
2018-10-20 23:07:46 -0300

    Fix typo

---
+++ tests/UntypedClass.hh
@@ -0,0 +1,11 @@
+<?hh // decl
+
+class UntypedSampleBaseClass {
+    public function untypedParameter($param): void {
+
+    }
+
+    public function untypedDefaultParameter($untyped = 'foo'): void {
+
+    }
+}
